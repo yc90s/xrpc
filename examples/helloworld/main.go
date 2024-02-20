f0f2dd4c9924dd4a2ebd119f6f94bc7f1549f223
yc90s <1050676515@qq.com>
2024-02-20 16:50:35 +0800

    add examples

---
+++ examples/helloworld/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"errors"
+	"flag"
+
+	"github.com/yc90s/xrpc"
+	natsmq "github.com/yc90s/xrpc/mq/nats"
+
+	"github.com/golang/glog"
+	"github.com/nats-io/nats.go"
+)
+
+type HelloRPCService struct {
+	*xrpc.RPCServer
+}
+
+func newHelloService() *HelloRPCService {
+	nc, err := nats.Connect("nats://127.0.0.1:4222", nats.MaxReconnects(1000))
+	if err != nil {
+		glog.Error("nats error ", err)
+	}
+
+	s := &HelloRPCService{
+		RPCServer: xrpc.NewRPCServer(
+			xrpc.SetMQ(natsmq.NewMQueen(nc)),
+			xrpc.SetSubj("hello_server"),
+		),
+	}
+	RegisterHelloServiceServer(s.RPCServer, s)
+	return s
+}
+
+func (s *HelloRPCService) Hello(request string) (string, error) {
+	reply := "hello:" + request
+	return reply, nil
+}
+
+func (s *HelloRPCService) HelloError(arg1 *string, arg2 string) (*string, error) {
+	return nil, errors.New("hello error")
+}
+
+func (s *HelloRPCService) Add(arg1 int, arg2 *int) (int, error) {
+	sum := arg1 + *arg2
+	return sum, nil
+}
+
+func (s *HelloRPCService) Ping() {
+	glog.Info("ping")
+}
+
+func (s *HelloRPCService) Bye(arg string) {
+	glog.Info("bye:" + arg)
+}
+
+func newHelloRPCServiceClient() *HelloServiceClient {
+	nc, err := nats.Connect("nats://127.0.0.1:4222", nats.MaxReconnects(1000))
+	if err != nil {
+		glog.Error("nats error ", err)
+	}
+
+	return NewHelloServiceClient(xrpc.NewRPCClient(
+		xrpc.SetMQ(natsmq.NewMQueen(nc)),
+		xrpc.SetSubj("hello_client"),
+	))
+}
+
+func main() {
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+	s := newHelloService()
+	err := s.Start()
+	if err != nil {
+		glog.Error(err)
+	}
+
+	c := newHelloRPCServiceClient()
+
+	reply1, err1 := c.Hello("hello_server", "yc90s")
+	if err1 != nil {
+		glog.Error(err1)
+	} else {
+		glog.Info(reply1)
+	}
+
+	err2 := c.Bye("hello_server", "yc90s")
+	if err2 != nil {
+		glog.Info(err2)
+	}
+
+	err3 := c.Ping("hello_server")
+	if err3 != nil {
+		glog.Info(err3)
+	}
+
+	var arg string = "123"
+
+	// test error
+	reply4, err4 := c.HelloError("hello_server", &arg, "456")
+	if err4 != nil {
+		glog.Error(err4)
+	} else {
+		glog.Info(*reply4)
+	}
+
+	num := 3
+	sum, err5 := c.Add("hello_server", 5, &num)
+	if err5 != nil {
+		glog.Info(err5)
+	} else {
+		glog.Info(sum)
+	}
+}
---
+++ examples/protobuf/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+
+	"github.com/yc90s/xrpc"
+	protocodec "github.com/yc90s/xrpc/codec/proto"
+	"github.com/yc90s/xrpc/examples/protobuf/pb"
+	natsmq "github.com/yc90s/xrpc/mq/nats"
+
+	"github.com/golang/glog"
+	"github.com/nats-io/nats.go"
+)
+
+type HelloRPCService struct {
+	*xrpc.RPCServer
+}
+
+func newHelloService() *HelloRPCService {
+	nc, err := nats.Connect("nats://127.0.0.1:4222", nats.MaxReconnects(1000))
+	if err != nil {
+		glog.Error("nats error ", err)
+	}
+
+	s := &HelloRPCService{
+		RPCServer: xrpc.NewRPCServer(
+			xrpc.SetMQ(natsmq.NewMQueen(nc)),
+			xrpc.SetSubj("hello_server"),
+			xrpc.SetCodec(protocodec.NewCodec()),
+		),
+	}
+	RegisterHelloServiceServer(s.RPCServer, s)
+	return s
+}
+
+func (s *HelloRPCService) Hello(request *pb.String) (*pb.String, error) {
+	reply := &pb.String{Value: "hello:" + request.GetValue()}
+	return reply, nil
+}
+
+func (s *HelloRPCService) Add(arg1 *pb.String, arg2 *pb.String) (*pb.String, error) {
+	reply := &pb.String{Value: arg1.GetValue() + arg2.GetValue()}
+	return reply, nil
+}
+
+func newHelloRPCServiceClient() *HelloServiceClient {
+	nc, err := nats.Connect("nats://127.0.0.1:4222", nats.MaxReconnects(1000))
+	if err != nil {
+		glog.Error("nats error ", err)
+	}
+
+	return NewHelloServiceClient(xrpc.NewRPCClient(
+		xrpc.SetMQ(natsmq.NewMQueen(nc)),
+		xrpc.SetSubj("hello_client"),
+		xrpc.SetCodec(protocodec.NewCodec()),
+	))
+}
+
+func main() {
+	flag.Set("logtostderr", "true")
+	flag.Parse()
+	s := newHelloService()
+	err := s.Start()
+	if err != nil {
+		glog.Error(err)
+	}
+
+	c := newHelloRPCServiceClient()
+
+	arg1 := &pb.String{Value: "yc90s"}
+	reply1, err1 := c.Hello("hello_server", arg1)
+	if err1 != nil {
+		glog.Error(err1)
+	} else {
+		glog.Info(reply1)
+	}
+
+	arg2 := &pb.String{Value: "123"}
+	reply3, err3 := c.Add("hello_server", arg2, arg2)
+	if err3 != nil {
+		glog.Info(err3)
+	} else {
+		glog.Info(reply3)
+	}
+}
